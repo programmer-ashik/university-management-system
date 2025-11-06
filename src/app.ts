a9499ba9f530a418897ea0592f56ee38642f18c0
ashik-coder <ashikhasan998@gmail.com>
2025-11-07 01:38:00 +0600

    text husky

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,21 @@
 // Import required modules
-import cors from "cors";
-import express, { Application } from "express";
-import router from "./app/modules/user/user.route";
+import cors from 'cors'
+import express, { Application } from 'express'
 
 // Create an Express application instance
-const app: Application = express();
+const app: Application = express()
 
 // Enable Cross-Origin Resource Sharing (CORS)
-app.use(cors());
+app.use(cors())
 
 // Parse incoming JSON requests
-app.use(express.json());
+app.use(express.json())
 
 // Parse URL-encoded data (e.g., from forms)
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
 //routing start from hare
 // Mount user-related routes under /api/v1/user
-app.use("/api/v1/user", router);
+// app.use("/api/v1/user", );
 
 // Export the configured Express application
-export default app;
+export default app
