3dc6bdd092919b286d0fc56ebfd7afef14b115f7
ashik-coder <ashikhasan998@gmail.com>
2025-11-08 14:55:40 +0600

    create user  model

---
+++ src/app/module/users/user.model.ts
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose'
+import { Schema } from 'mongoose'
+import { IUser, UserModel } from './users.interface'
+
+const userScheme = new Schema<IUser>(
+  {
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    role: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+)
+export const user = mongoose.model<IUser, UserModel>('User', userScheme)
